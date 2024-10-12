5e3b9d56a84b264e26179e0595f19885b11ed25a
lindagd <liwdag@gmail.com>
2024-10-12 17:15:14 -0300

    Cadastrando galpões e recebendo feedback de sucesso

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -2,4 +2,23 @@ class WarehousesController < ApplicationController
   def show
     @warehouse = Warehouse.find(params[:id])
   end
+
+  def new
+    @warehouse = Warehouse.new
+  end
+
+  def create
+    params_warehouse = params.require(:warehouse).permit(:name,
+                          :description, :code, :address,
+                          :city, :cep, :area)   
+    
+    warehouse = Warehouse.new(params_warehouse)
+    
+    if warehouse.save
+      flash[:notice] = 'Galpão cadastrado com sucesso'
+      return redirect_to root_path
+    end
+    
+
+  end
 end
